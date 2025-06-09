be40e4d8e6e8c787e95248bed23992544c4a35cc
lucunji <lucunjif@gmail.com>
2025-06-09 00:04:33 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# OneBlockOneClick
+
+Adds a little delay after breaking the first block, so you won't accidentally break the next block with an instant mining tool.
+
+<!-- Add a gif here that compares behavior with/without this mod -->
---
+++ common/src/main/java/io/github/lucunji/oneclickoneblock/Constants.java
@@ -0,0 +1,11 @@
+package io.github.lucunji.oneclickoneblock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "oneclickoneblock";
+    public static final String MOD_NAME = "OneClickOneBlock";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
